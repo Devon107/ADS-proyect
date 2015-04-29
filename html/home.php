beb5f750766b7ed28a8bfdca08438e79db5d4b24
Abinadi <abiclank@gmail.com>
2015-04-29 16:46:08 -0600

    Update home.php

--- html/home.php
+++ html/home.php
@@ -1,71 +1,10 @@
 <?php
-session_start();
-if (isset($_SESSION["usuario"]))
-{
-  include('Usuario.php');
+ $titulo = "HOME";
+  include("./base/header.php"); 
 ?>
-
-<!doctype html>
-<html class="no-js" lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>NYMSA | Home</title>
-    <link rel="stylesheet" href="../css/foundation.css" />
-    <script src="js/vendor/modernizr.js"></script>
-  <script src="../js/shortcut.js" type="text/javascript"></script>
-  </head>
-  <body>
-  
 <div class="row">
-  <div class="large-12 columns">
- 
-     
-      <nav class="top-bar" data-topbar>
-        <ul class="title-area">
-           
-          <li class="name">
-            <h1>
-              <a href="#">
-                NYMSA
-              </a>
-            </h1>
-          </li>
-          <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
-        </ul>
-        <section class="top-bar-section">		
-		<nav class="top-bar" data-topbar role="navigation"> 
-		<ul class="title-area"> 
-		<li class="name">
-		<h1><a href="#">NYMSA</a></h1> 
-		</li> <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone --> 
-		<li class="toggle-topbar menu-icon">
-		<a href="#"><span>Menu</span></a></li>
-		</ul> 
-		<section class="top-bar-section"> <!-- Right Nav Section --> <ul class="right">
-		<li class="active"><a href="#"><?php echo $usuario ?></a></li>
-		<li class="has-dropdown"> 
-		<a href="#"><img class="icono" src="../static/icons/settings-icon2.png"/></a> 
-		<ul class="dropdown"> 
-		<li><a href="cuenta.php">Cuenta</a></li> 
-		<li class="active"><a href="logout.php">Cerrar Sesión</a></li>
-		</ul> 
-		</li> </ul> <!-- Left Nav Section --> <ul class="left"> 
-		</ul> 
-		</section>
-		</nav>
-		
-        </section>
-      </nav>
-    </div>
-  </div>
- 
-  <div class="row">
     <div class="large-12 columns">
- 
-     
-
-    <div class="row">
+ <div class="row">
       <div class="small-12 show-for-small"><br>
         <img src="http://placehold.it/1000x600&text=For Small Screens"/>
       </div>
@@ -94,22 +33,8 @@ if (isset($_SESSION["usuario"]))
       </div>
     </div>
   </div>
-   <footer class="row">
-    <div class="large-12 columns">
-      <hr>
-      <div class="row">
-        <div class="large-6 columns" align="center">
-          <p >© NYMSA DERECHOS RESERVADOS</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-    
+   <?php
+  include('./base/footer.php');
+  ?>
   </body>
 </html>
-<?php
-}
-else{
-	header('Location: index.php');
-}
-?>
