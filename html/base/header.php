07386ad7faea3c832d0a2948eef11270f28fbaaf
Abinadi <abiclank@gmail.com>
2015-04-29 16:44:48 -0600

    Update header.php
    arreglando menu

--- html/base/header.php
+++ html/base/header.php
@@ -4,16 +4,18 @@ if (isset($_SESSION["usuario"]))
 {
   include('Usuario.php');
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html class="no-js" lang="en">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title><?php echo $titulo; ?></title>
-    <link rel="stylesheet" href="../css/foundation.css" />
-    <script src="../js/vendor/modernizr.js"></script>	
-	<script src="../js/shortcut.js" type="text/javascript"></script>
-    <script>var sep = new Array(4,4)
+<title><?php echo $titulo; ?></title>
+  <link rel="stylesheet" href="../css/style.css" type="text/css">
+    <link rel="stylesheet" href="../css/foundation.css"/>
+  <link rel="stylesheet" href="../css/Tablas_reportes.css">
+    <script src="js/vendor/modernizr.js"></script>
+  <script src="../js/shortcut.js" type="text/javascript"></script>
+      <script>var sep = new Array(4,4)
 var sep2 = new Array(4,6,3,1)
 var sep3 = new Array(8,1)
 function mascara(d,sep,pat,nums){
@@ -55,7 +57,7 @@ if(d.valant != d.value){
 }</script>
   </head>
   <body>
-	<div id="header">
+    <section class="hero">
 <div class="row">
   <div class="large-12 columns">  
       <nav class="top-bar" data-topbar>
@@ -63,7 +65,7 @@ if(d.valant != d.value){
            
           <li class="name">
             <h1>
-              <a href="home.php">
+              <a href="#">
                 NYMSA
               </a>
             </h1>
@@ -72,28 +74,32 @@ if(d.valant != d.value){
         </ul>
  
         <section class="top-bar-section">
-		
-		<nav class="top-bar" data-topbar role="navigation"> 
-		<ul class="title-area"> 
-		<li class="name">
-		<h1><a href="home.php">NYMSA</a></h1> 
-		</li> <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone --> 
-		<li class="toggle-topbar menu-icon">
-		<a href="#"><span>Menu</span></a></li>
-		</ul> 
-		<section class="top-bar-section"> <!-- Right Nav Section --> <ul class="right">
-		<li class="active"><a href="#"><?php echo $usuario ?></a></li>
-		<li class="has-dropdown"> 
-		<a href="#"><img class="icono" src="../static/icons/settings-icon2.png"/></a> 
-		<ul class="dropdown"> 
-		<li><a href="cuenta.php">Cuenta</a></li> 
-		<li class="active"><a href="logout.php">Cerrar Sesi&oacute;n</a></li>
-		</ul> 
-		</li> </ul> <!-- Left Nav Section --> <ul class="left"> 
-		</ul> 
-		</section>
-		</nav>
-		
+    
+    <nav class="top-bar" data-topbar role="navigation"> 
+    <ul class="title-area"> 
+      <li class="name">
+        <h1><a href="home.php">NYMSA</a>
+      </h1> 
+      </li> <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone --> 
+      <li class="toggle-topbar menu-icon">
+        <a href="#"><span>Menu</span></a>
+      </li>
+    </ul> 
+      <section class="top-bar-section"> <!-- Right Nav Section --> <ul class="right">
+      <li class="active">
+        <a href="#"><?php echo $usuario ?></a>
+      </li>
+      <li class="has-dropdown"> 
+      <a href="#"><img class="icono" src="../static/icons/settings-icon2.png"/></a> 
+      <ul class="dropdown"> 
+      <li><a href="cuenta.php">Cuenta</a></li> 
+      <li class="active"><a href="logout.php">Cerrar Sesi&oacute;n</a></li>
+    </ul> 
+    </li> </ul> <!-- Left Nav Section --> <ul class="left"> 
+    </ul> 
+    </section>
+    </nav>
+    
         </section>
       </nav>
     </div>
