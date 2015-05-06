ca51892ec1c9ed40a8e89c7aaa5253e1d3b44fc1
Abinadi <abiclank@gmail.com>
2015-05-06 16:01:52 -0600

    Update ActualizarCobros.php

--- html/ActualizarCobros.php
+++ html/ActualizarCobros.php
@@ -22,96 +22,16 @@
 	$monto=$resultado["Monto"];	
 	$telefono=$resultado["Telefono"];	
 	$estado=$resultado["Estado"];	
-	echo $estado;
+	//echo $estado;
 	if($estado=='1')
 		$estado="checked";
 	else
 		$estado="";
+
+	
+	include('./base/header.php');
+	include('./base/menu.php');
 	  ?>
-<!DOCTYPE html>
-<html class="no-js" lang="en">
- <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>NYMSA | Actualizar Cobro</title>
-    <link rel="stylesheet" href="../css/foundation.css" />
-    <script src="js/vendor/modernizr.js"></script>
-    <script src="../js/validarguion.js"></script>
-  <script src="../js/shortcut.js" type="text/javascript"></script>
-
-  </head>
-<body>
-	<div class="row">
-  <div class="large-12 columns">
- 
-     
-      <nav class="top-bar" data-topbar>
-        <ul class="title-area">
-           
-          <li class="name">
-            <h1>
-              <a href="#">
-                NYMSA
-              </a>
-            </h1>
-          </li>
-          <li class="toggle-topbar menu-icon"><a href="#"><span>menu</span></a></li>
-        </ul>
- 
-        <section class="top-bar-section">
-		
-		<nav class="top-bar" data-topbar role="navigation"> 
-		<ul class="title-area"> 
-		<li class="name">
-		<h1><a href="home.php">NYMSA</a></h1> 
-		</li> <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone --> 
-		<li class="toggle-topbar menu-icon">
-		<a href="#"><span>Menu</span></a></li>
-		</ul> 
-		<section class="top-bar-section"> <!-- Right Nav Section --> <ul class="right">
-		<li class="active"><a href="#"><?php echo "usuario" ?></a></li>
-		<li class="has-dropdown"> 
-		<a href="#"><img class="icono" src="../static/icons/settings-icon2.png"/></a> 
-		<ul class="dropdown"> 
-		<li><a href="cuenta.php">Cuenta</a></li> 
-    <li class="active"><a href="logout.php">Cerrar Sesión</a></li>
-		</ul> 
-		</li> </ul> <!-- Left Nav Section --> <ul class="left"> 
-		</ul> 
-		</section>
-		</nav>
-		
-        </section>
-      </nav>
- 
-     
-  
-    </div>
-  </div>
-
-  <a name="Top">
-   <div class="row">
-    <div class="large-12 columns">
-      <div class="panel">
-        <center><h1>Cobros</h1></center>
-      </div>
-    </div>
-  </div>	
-  
-  <div class="row">
-    <div class="large-3 columns">
-      <h1><img src="../img/logo.png"/></h1>
-    </div>
-    <div class="large-9 columns">
-      <ul class="inline-list right">
-        <li><a href="#">Home</a></li>
-        <li><a href="#Top">Desplazar hacia arriba</a></li>
-		<li><a href="#primernombre">Desplazar el centro</a></li>
-        <li><a href="#Bot">Desplazar hacia abajo</a></li>
-
-      </ul>
-    </div>
-  </div>
 
   <div class="row">    
    <div class="large-9 push-3 columns">
@@ -157,41 +77,5 @@
 		</table>
 	</form>
 	</div>
-    
-    
-    <div class="large-3 pull-9 columns">
-        
-      <ul class="side-nav">
-        <li><a href="#">Home</a></li>
-        <li><a href="#Top">Desplazar hacia arriba</a></li>
-		<li><a href="#primernombre">Desplazar el centro</a></li>
-        <li><a href="#Bot">Desplazar hacia abajo</a></li>
-      </ul>
-      
-      <p><img src="../img/addnymsa2.jpg"/></p>
-        
-    </div>
-  </div>
-
-  <footer class="row">
-    <div class="large-12 columns">
-      <hr/>
-      <div class="row">
-        <div class="large-6 columns">
-          <p>© Copyright Negocios y Mas S.A. de C.V.</p>
-        </div>
-        <div class="large-6 columns">
-          <ul class="inline-list right">
-        <li><a href="#">Home</a></li>
-        <li><a href="#Top">Desplazar hacia arriba</a></li>
-		<li><a href="#primernombre">Desplazar hacia el centro</a></li>
-
-          </ul>
-        </div>
-      </div>
-    </div> 
-  </footer>
-   
-    <a name="Bot">
-</body>
-</html>
+	<?php
+include('./base/footer.php');?>
