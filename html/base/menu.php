9dfce0a00981c09152607a9bec7e06d68a4f62e9
Abinadi <abiclank@gmail.com>
2015-05-06 16:00:33 -0600

    Update menu.php

--- html/base/menu.php
+++ html/base/menu.php
@@ -1,13 +1,1 @@
-  <div class="row">
-    <div class="large-3 columns">
-      <h1><img src="../img/logo.png"/></h1>
-    </div>
-    <div class="large-9 columns">
-      <ul class="inline-list right">
-        <li><a href="#">Home</a></li>
-        <li><a href="#Top">Desplazar hacia arriba</a></li>
-		<li><a href="#primernombre">Desplazar el centro</a></li>
-        <li><a href="#Bot">Desplazar hacia abajo</a></li>
-      </ul>
-    </div>
-  </div>
+
