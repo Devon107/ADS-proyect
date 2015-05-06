8bdedb94106369dc565f8fd50d1a5020fb5a6dfa
Abinadi <abiclank@gmail.com>
2015-05-06 16:00:59 -0600

    Update footer.php

--- html/base/footer.php
+++ html/base/footer.php
@@ -3,14 +3,7 @@
       <hr/>
       <div class="row">
         <div class="large-6 columns">
-          <p>© Copyright Negocios y Mas S.A. de C.V.</p>
-        </div>
-        <div class="large-6 columns">
-          <ul class="inline-list right">
-        <li><a href="#">Home</a></li>
-        <li><a href="#Top">Desplazar hacia arriba</a></li>
-		<li><a href="#primernombre">Desplazar hacia el centro</a></li>
-          </ul>
+          <p>Â© Copyright Negocios y Mas S.A. de C.V.</p>
         </div>
       </div>
     </div>
